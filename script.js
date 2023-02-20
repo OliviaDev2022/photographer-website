533e33a2e6f91cb7f8b1b4e1d0e64383022adbd8
Olivia QUENUM <olivia.quenum@gmail.com>
2023-02-20 20:46:09 +0100

    structure

---
+++ script.js
@@ -0,0 +1,23 @@
+let url = $(this).attr('href');
+
+// Récupérer les liens des categories de photos
+let ToutVoir = document.getElementById('#categorie');
+let bapteme = document.getElementById('#bapteme');
+let bébé = document.getElementById('#bébé');
+let couple = document.getElementById('#couple');
+let famille = document.getElementById('#famille');
+let grossesse = document.getElementById('#grossesse');
+let mariage = document.getElementById('#mariage');
+let portrait = document.getElementById('#portrait');
+
+// Récuperer les photos contenues dans chaque catégorie
+let AllPhotos = document.getElementById('#toutesphotos');
+let PhotosBapteme = document.getElementById('#photosbapteme');
+let PhotosBébé = document.getElementById('#photosbébé');
+let PhotosCouple = document.getElementById('#photoscouple');
+let PhotosFamille = document.getElementById('#photosfamille');
+let PhotosGrossesse = document.getElementById('#photosgrossesse');
+let PhotosMariage = document.getElementById('#photosmariage');
+let PhotosPortrait = document.getElementById('#photosportrait');
+
+
