b2d227dc028b4eba8063c3d98a004d3064e10590
Olivia QUENUM <olivia.quenum@gmail.com>
2023-02-23 11:39:55 +0100

    filtre galerie photos

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-let url = $(this).attr('href');
+// CODE ET EXPLICATIONS
+// Dans le html j'ai créé une div contenant toutes le photos de la galerie, subdivisée en plusieurs divs 
+// avec des id liés à la catégorie de photos qu'on veut voir. 
+// L'idée est d'afficher la div #toutesphotos par défaut quand l'utilisateur arrive sur la page. 
+// Ensuite, au clic sur chaque catégorie, la div de photos correspondante apparait.
+// j'essaye avec la div #photosbapteme pour commencer mais le code ne fonctionne deja pas. 
+// Comment le corriger puis le factoriser pour tous les autres liens ?
 
-// Récupérer les liens des categories de photos
-let ToutVoir = document.getElementById('#categorie');
-let bapteme = document.getElementById('#bapteme');
-let bébé = document.getElementById('#bébé');
-let couple = document.getElementById('#couple');
-let famille = document.getElementById('#famille');
-let grossesse = document.getElementById('#grossesse');
-let mariage = document.getElementById('#mariage');
-let portrait = document.getElementById('#portrait');
 
-// Récuperer les photos contenues dans chaque catégorie
-let AllPhotos = document.getElementById('#toutesphotos');
-let PhotosBapteme = document.getElementById('#photosbapteme');
-let PhotosBébé = document.getElementById('#photosbébé');
-let PhotosCouple = document.getElementById('#photoscouple');
-let PhotosFamille = document.getElementById('#photosfamille');
-let PhotosGrossesse = document.getElementById('#photosgrossesse');
-let PhotosMariage = document.getElementById('#photosmariage');
-let PhotosPortrait = document.getElementById('#photosportrait');
+// Récupérer tous les liens affichant les catégories de photos
+const links = document.querySelectorAll('.photos a');
 
+// Au "clic" sur chaque lien
+links.forEach(link => {
+  link.addEventListener('click', function(event) {
+    // Empêcher le navigateur de quitter la page
+    event.preventDefault();
+
+    // Récupérer l'id de la catégorie du lien cliqué par l'utilisateur
+    const category = this.getAttribute('id');
+
+    // Afficher les images correspondantes à chaque catégorie sélectionnée selon l'id
+    if (category === 'bapteme') {
+     //Masquer toutes les images
+     const images = document.getElementById('#toutesphotos');
+      images.forEach(image => {
+       image.style.display = 'none';
+      });
+     // puis afficher celles de #photosbapteme
+      let PhotosBapteme = document.getElementById('#photosbapteme');
+      PhotosBapteme.forEach(PhotosBapteme => {
+       PhotosBapteme.style.display = 'block';
+       });
+     } 
+  });
+});
 
